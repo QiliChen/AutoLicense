9808e50ca7c362bbf508c98f84dbc576b477877d
Chenyiwei <chenyiwei10@hotmail.com>
2024-03-19 10:04:16 +0800

    init:add Auto script

---
+++ AutoJetBrainsLicense.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import requests
+from bs4 import BeautifulSoup
+
+
+def readHtml():
+    url = 'https://www.idejihuo.com/'
+    response = requests.get(url)
+    unique_values = set()
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        inputs = soup.find_all('input')
+        for input_element in inputs:
+            value = input_element.get('value')
+            if value is not None:
+                unique_values.add(value)
+    else:
+        print(f'Failed to retrieve the webpage. Status code: {response.status_code}')
+
+    return unique_values
+
+def writeDoc(licenses):
+    with open('JetBrainsLicense.txt', 'w') as file:
+        for license in licenses:
+            file.write(license + '\n')
+
+if __name__ == '__main__':
+    licenses = readHtml()
+    writeDoc(licenses)
